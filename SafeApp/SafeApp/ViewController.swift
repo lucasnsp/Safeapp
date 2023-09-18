f54ead1cd69ae2ea6a8f8a41db50dfb9cfb51d47
Lucas Neves <lucas.neves12@icloud.com>
2023-09-18 09:32:59 -0300

    SafeApp creation

---
+++ SafeApp/SafeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeApp
+//
+//  Created by Lucas Neves dos santos pompeu on 18/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
